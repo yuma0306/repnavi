3f9afcdf6d10be38d7a8f59a814740b4a866531f
yuma0306 <yuma0306895@gmail.com>
2024-03-16 17:11:49 +0900

    管理画面ディレクトリ変更

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,7 +18,7 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
-        return view('profile.edit', [
+        return view('owner.profile.edit', [
             'user' => $request->user(),
         ]);
     }
